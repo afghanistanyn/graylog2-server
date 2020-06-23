77233cecdc63c53af773267c540121850418e689
Dennis Oelkers <dennis@graylog.com>
2020-06-23 15:16:08 +0200

    Replacing usage of `waitForElement` in `WidgetErrorBoundary`.

--- graylog2-web-interface/src/views/components/widgets/WidgetErrorBoundary.test.jsx
+++ graylog2-web-interface/src/views/components/widgets/WidgetErrorBoundary.test.jsx
@@ -1,18 +1,18 @@
 // @flow strict
 import * as React from 'react';
-import { render, waitForElement } from 'wrappedTestingLibrary';
+import { render } from 'wrappedTestingLibrary';
 import asMock from 'helpers/mocking/AsMock';
 
 import WidgetErrorBoundary from './WidgetErrorBoundary';
 
 describe('WidgetErrorBoundary', () => {
   it('renders children if no error is thrown', async () => {
-    const { getByText } = render((
+    const { findByText } = render((
       <WidgetErrorBoundary>
         <div>Hello World!</div>
       </WidgetErrorBoundary>
     ));
-    await waitForElement(() => getByText('Hello World!'));
+    await findByText('Hello World!');
   });
 
   it('renders helpful error message if child throws error', async () => {
@@ -21,13 +21,13 @@ describe('WidgetErrorBoundary', () => {
     asMock(console.error).mockImplementation(jest.fn());
 
     const Component = () => { throw new Error('The dungeon collapses, you die!'); };
-    const { getByText } = render((
+    const { findByText } = render((
       <WidgetErrorBoundary>
         <Component />
       </WidgetErrorBoundary>
     ));
-    await waitForElement(() => getByText('While rendering this widget, the following error occurred:'));
-    await waitForElement(() => getByText('Error: The dungeon collapses, you die!'));
+    await findByText('While rendering this widget, the following error occurred:');
+    await findByText('Error: The dungeon collapses, you die!');
 
     // eslint-disable-next-line no-console
     asMock(console.error).mockRestore();
